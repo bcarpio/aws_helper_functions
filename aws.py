d3fa4ae14a5297c3bc3d5c8a7e36d2726ad4f872
Brian Carpio <bcarpio@itriagehealth.com>
2014-09-06 07:00:05 -0600

    added support to search by instance ID

--- aws.py
+++ aws.py
@@ -44,7 +44,6 @@ def get_instance_list(region):
             environment = "None"
         print name+","+environment
 
-
 @task
 def get_iam_users():
     config = get_config()
@@ -52,3 +51,12 @@ def get_iam_users():
     users = conn.get_all_users()
     for user in users['list_users_response']['list_users_result']['users']:
         print user['user_name']+","+user['create_date']
+
+@task
+def get_host_name(region,id):
+    conn = ec2_connect(region)
+    instances = conn.get_all_instances()
+    for instance in instances:
+        if id == instance.instances[0].id:
+            print instance.instances[0].__dict__['tags']['Name']
+            break
